c67454e671cd066e83267712178ce734e75e6daa
Aditya <adityanaag91@gmail.com>
2016-06-23 02:15:39 +0530

    stores cart item in local storage while adding items

--- controllers/itemsStoreController.js
+++ controllers/itemsStoreController.js
@@ -10,15 +10,18 @@
 	  	$scope.items = Items().getNewInstance();
 	  	$scope.cart = Cart().getNewInstance();
 	  	$scope.items.getItems();
+	  	var localStorageKey = 'MasalaAndSpices';
 	  	$scope.addOne = function (itemId) {
 	  		$scope.cart.itemsById[itemId].quantity += 1;
-	  		$scope.cart.totalPrice += $scope.cart.itemsById[itemId].price; 
+	  		$scope.cart.totalPrice += $scope.cart.itemsById[itemId].price;
+	  		localStorage.setItem(localStorageKey, JSON.stringify($scope.cart)); 
 	  	}
 	  	$scope.removeOne = function (itemId) {
 	  		var itemQuantity = $scope.cart.itemsById[itemId].quantity;
 	  		itemQuantity = itemQuantity - 1;
 	  		$scope.cart.itemsById[itemId].quantity = itemQuantity;
-	  		$scope.cart.totalPrice -= $scope.cart.itemsById[itemId].price; 
+	  		$scope.cart.totalPrice -= $scope.cart.itemsById[itemId].price;
+	  		localStorage.setItem(localStorageKey, JSON.stringify($scope.cart));  
 	  		if (!itemQuantity) {
 	  			$scope.cart.removeItem(itemId);
 	  		}
